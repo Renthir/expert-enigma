5b0165e5a18d750b6bb763b6197662f8ef9d673d
Seth Reid <reid.seth.96@gmail.com>
2023-05-22 11:28:58 -0600

    finished creation cruds, added server routes

--- crud.py
+++ crud.py
@@ -2,25 +2,43 @@
 
 from model import User, Character, Background, Inv_Armor, Armor, Inv_Wep, Weapon
 
-#functions
+#creation functions
 
 def create_user(email, password):
-    user = User(email=email, password=password)
+    user = User(email, password)
     return user
 
 def create_char(char_name, user_id, background_id, bio, descrip):
-    char = Character(char_name=char_name, user_id=user_id, background_id=background_id, bio=bio, descrip=descrip)
+    char = Character(char_name, user_id, background_id, bio, descrip)
     return Character
 
 def create_background(background_name, descrip, rp_boon):
-    background = Background(background_name=background_name, descrip=descrip, rp_boon=rp_boon)
+    background = Background(background_name, descrip, rp_boon)
     return background
 
 def create_weapon(wep_name, wep_dmg, wep_range, wep_price):
-    weapon = Weapon(wep_name=wep_name, wep_dmg=wep_dmg, wep_range=wep_range, wep_price=wep_price)
+    weapon = Weapon(wep_name, wep_dmg, wep_range, wep_price)
     return weapon
 
 def create_armor(armor_name, armor_stat, armor_price):
-    armor = Armor(armor_name=armor_name, armor_stat=armor_stat, armor_price=armor_price)
+    armor = Armor(armor_name, armor_stat, armor_price)
     return armor
 
+def create_inv_weapon(wep_id, char_id, qty):
+    inv_weapon = Inv_Wep(wep_id, char_id, qty)
+    return inv_weapon
+
+def create_inv_armor(armor_id, char_id, qty):
+    inv_armor = Inv_Armor(armor_id, char_id, qty)
+    return inv_armor
+
+
+# fetching functions
+
+def get_armors():
+    """Return all Armor"""
+    return Armor.query.all()
+
+def get_weapons():
+    """Return all weapons"""
+    return Weapon.query.all
--- server.py
+++ server.py
@@ -1,1 +1,42 @@
-from flask import Flask
+"""Server for Sci-fi TTRPG Character tracker"""
+
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db, db 
+import crud
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = 'dev'
+app.jinja_env.undefined = StrictUndefined
+
+
+# Routes
+
+@app.route("/")
+def homepage():
+    """View homepage"""
+    return render_template("home.html")
+
+
+# @app.route("/login")
+# def login():
+
+
+@app.route("/armors")
+def armors_page():
+    """View Armor list"""
+    return render_template("armors.html")
+
+@app.route("/weapons")
+def weapons_page():
+    """View Weapons list"""
+    return render_template("weapons.html")
+
+@app.route("/characters")
+def characters_page():
+    """Page that shows a user's characters and the option to create a new one"""
+    #check session id to see if user is logged in, provide user's characters
+    return render_template("characters.html")
+
+
